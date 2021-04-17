3a6095e3d108291f0e6092c13b31b9d3b97c19f8
cksgnlcjswo <cksgnlcjswoo@naver.com>
2021-04-17 12:22:15 +0900

    에러 처리

--- accountHandler.py
+++ accountHandler.py
@@ -1,5 +1,5 @@
 """
-version 1.3.1
+version 1.3.2
 작성자 : 김찬휘
 이메일 : cksgnlcjswoo@naver.com
 description : accountHandler 클래스 선언이 명시되어있다.
@@ -44,13 +44,39 @@ class AccountHandler:
         name = input()
         print("money : ",end='')
         money = int(input())
-        print("interate : ",end='')
+        print("interate(0~10% 사이) : ",end='')
         interate = int(input())
         print()
 
+        #잘못된 유저 아이디 형식
+        if False == self.checkValidAccount(id,name,money,interate): return
+            
         self.accList.append(normalAccount.NormalAccount(id,money,name,interate))
         self.accNum += 1
         return
+
+    def checkValidAccount(self,id,name,money,interate):
+        #id가 입력되지 않은 경우
+        if(len(id) == 0):
+            print("id is not defined.")
+            return False
+
+        #이름이 입력되지 않은 경우
+        if(len(name) == 0):
+            print("please input your name")    
+            return False
+
+        #돈이 -인경우
+        if(money < 0):
+            print("money cannot be minus")
+            return False
+
+        #이율은 10%를 넘길 수 없다.
+        if interate > 11 and interate < 0 :
+            print("interate shoud be in 0~10%")
+            return False
+
+        return True    
 
     def makeCreditAccount(self):
         print("making credit card type...")
@@ -60,11 +86,13 @@ class AccountHandler:
         name = input()
         print("money : ",end='')
         money = int(input())
-        print("interate : ",end='')
+        print("interate(0~10% 사이) : ",end='')
         interate = int(input())
         print("credit level number(숫자로 입력하세요: Ato9, Bto7, Cto5, Dto3, Eto1) : ",end='')
         level = int(input())
         print()
+
+        if False == self.checkValidAccount(id,name,money,interate): return
 
         #1등급
         if level == enumClass.Credit.LEVEL_A.value:
